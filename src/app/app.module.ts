731e9e57afe46956825ff5d4bb53e9bd78828d3e
sarusanhueza <sarusanhueza2@gmail.com>
2023-08-17 11:17:26 -0300

    conctamos la base de datos de firebese

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 // componentes globales
 import { SharedModule } from './shared/shared.module';
+// firebasa. Nos conectamos con la base de datos y nos trae los modulos necesarios.
+import { environment } from 'src/environments/environment';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+
+
 
 @NgModule({
   declarations: [
@@ -15,7 +22,10 @@ import { SharedModule } from './shared/shared.module';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    SharedModule
+    SharedModule,
+    AngularFireModule,
+    AngularFireAuthModule,
+    AngularFireStorageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: false,
+// pegamos el codigo de firebase SDK
+// borramos el const, ponemos dos punts luego de config y eliminamos el punto y coma final.
+  firebaseConfig: {
+    apiKey: "AIzaSyA1i92Fm12g-ZF7JK4m6a0Dlst9I91URYQ",
+    authDomain: "prueba-b6a92.firebaseapp.com",
+    projectId: "prueba-b6a92",
+    storageBucket: "prueba-b6a92.appspot.com",
+    messagingSenderId: "211811915311",
+    appId: "1:211811915311:web:6322a696604b74988c18da",
+    measurementId: "G-NX1MW25TNJ"
+  }
+}
